74ccbb253c98505afbdf8f0e7a96b9cd38745fe0
AniqqKhaizii <55343638+AniqqKhaizii@users.noreply.github.com>
2023-09-14 02:01:03 +0800

    Update init.R

--- init.R
+++ init.R
@@ -2,8 +2,8 @@
 #
 # Example R code to install packages if not already installed
 #
-my_packages = c("DT", "dplyr", "fpp2", "forecast", "ggplot2", "httr", "jsonlite", "lubridate", "openxlsx", "plotly", "readr", "readxl", "shiny", "shinydashboard",
-                "shinydashboardPlus", "shinyWidgets","TTR", "tidyr", "tibble", "likert", "bslib", "dashboardthemes", "highcharter", "leaflet", "textshaping")
+my_packages = c("DT", "dplyr", "ggplot2", "httr", "jsonlite", "lubridate", "openxlsx", "plotly", "readr", "readxl", "shiny", "shinydashboard",
+                "shinydashboardPlus", "tidyr" "dashboardthemes", "highcharter")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
